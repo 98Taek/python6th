458a3e5537db8ebbcc813fe09ebb525393f4adff
Jeong Taek <imjt9345@naver.com>
2023-05-30 13:03:06 +0900

    데이터 타입 실습

--- HTML-CSS실습/default.js
+++ HTML-CSS실습/default.js
@@ -10,3 +10,19 @@ console.error('Welcome JS Programs');
 alert('Welcome JS Programs');
 var a = prompt('Welcome JS Programs');
 console.log(a);
+
+console.log(typeof 123);
+console.log(typeof 123.5);
+console.log(typeof "123");
+console.log(typeof true);
+console.log(typeof false);
+
+var car;
+console.log(typeof car);
+var car = "";
+console.log(typeof car);
+var person = { firstName: "John", lastName:"Doe", age: 50, eyeColor: "blue"};
+
+console.log(typeof person, person);
+person = null;
+console.log(typeof person, person);
