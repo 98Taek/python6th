7e5eb149027d5c97bc3903a7818fca6c4c8c8da1
Jeong Taek <imjt9345@naver.com>
2023-05-30 11:24:11 +0900

    출력함수 실습

--- HTML-CSS실습/default.js
+++ HTML-CSS실습/default.js
@@ -1,1 +1,12 @@
-        document.write('Hello World 2');
+document.write('Hello World 2');
+document.write("<h1> Welcome to JS Programs </h1>");
+document.write("<h2> Welcome to JS Programs </h2>");
+
+console.log('Welcome JS Programs');
+console.info('Welcome JS Programs');
+console.warn('Welcome JS Programs');
+console.error('Welcome JS Programs');
+
+alert('Welcome JS Programs');
+var a = prompt('Welcome JS Programs');
+console.log(a);
