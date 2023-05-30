5cd38597a30c52564762590b34488336e9560069
Jeong Taek <imjt9345@naver.com>
2023-05-30 14:05:19 +0900

    문자열 함수 실습

--- HTML-CSS실습/default.js
+++ HTML-CSS실습/default.js
@@ -50,3 +50,23 @@ var sum = num1 + num2;
 console.log(num1 + num2);
 console.log("" + num1 + num2);
 console.log(num1 + " + " + num2 + " = " + sum);
+
+var text = prompt("Enter your Name: ");
+document.write("Your name: " + text + "<br/>");
+
+var len = text.length;
+document.write("Number of characters: " + len + "<br/>");
+
+document.write(text.charAt(2) + "<br/>");
+
+document.write(text.toUpperCase() + "<br/>");
+document.write(text.toLowerCase() + "<br/>");
+
+var text1 = "hi, ";
+var text2 = "bye";
+var text3 = text1.concat(text2);
+document.write(text3 + "<br/>");
+
+var text4 = "hello";
+var result = text4.slice(0,2);
+document.write(result + "<br/>");
