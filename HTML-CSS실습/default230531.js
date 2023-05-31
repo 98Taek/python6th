04d9ec02522780deef24d317049183a3a4f34624
Jeong Taek <imjt9345@naver.com>
2023-05-31 09:38:18 +0900

    연산자 실습

--- HTML-CSS실습/default230531.js
+++ HTML-CSS실습/default230531.js
@@ -118,3 +118,27 @@ var height = parseFloat(prompt("Enter height: "));
 var area = base * height * 0.5;
 
 document.write("Area of triangle: " + area + "<br/>");
+
+var num1 = 20;
+var num2 = 10;
+var num3 = "10";
+var num4 = 20;
+var num5 = 15;
+
+console.log('일반 크기비교');
+console.log(num1 > num2, num1, '>', num2);
+console.log(num1 >= num2, num1, '>=', num2);
+console.log(num1 < num2, num1, '<', num2);
+console.log(num1 <= num2, num1, '<=', num2);
+
+console.log('같은지 여부 확인');
+console.log(num1 == num4, num1, '==', num4);
+console.log(num1 != num4, num1, '!=', num4);
+
+console.log('===');
+console.log(num1 === num3, num1, '===', num3);
+console.log(num2 === num3, num2, '===', num3);
+console.log(num2 == num3, num2, '==', num3);
+
+console.log('num1 > num2 && num1 < num5', num1 > num2 && num1 < num5);
+console.log('num1 > num2 || num1 < num5', num1 > num2 || num1 < num5);
