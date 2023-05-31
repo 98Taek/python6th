197485aa29cfddef827792c7349d7053203f6ecd
Jeong Taek <imjt9345@naver.com>
2023-05-31 11:18:10 +0900

    숫자 영어 표기 실습

--- HTML-CSS실습/default230531.js
+++ HTML-CSS실습/default230531.js
@@ -165,13 +165,50 @@
 //} else {
 //    console.log('같은 수');
 //}
+//
+//var letter = prompt('Enter a letter: ');
+//
+//letter = letter.toLowerCase();
+//
+//if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+//    console.log('Vowel');
+//} else {
+//    console.log('Consonant');
+//}
 
-var letter = prompt('Enter a letter: ');
+var digit = parseInt(prompt('Enter a digit: '));
 
-letter = letter.toLowerCase();
-
-if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
-    console.log('Vowel');
-} else {
-    console.log('Consonant');
+switch(digit) {
+    case 0:
+        document.write('Zero');
+        break;
+    case 1:
+        document.write('One');
+        break;
+    case 2:
+        document.write('Two');
+        break;
+    case 3:
+        document.write('Three');
+        break;
+    case 4:
+        document.write('Four');
+        break;
+    case 5:
+        document.write('Five');
+        break;
+    case 6:
+        document.write('Six');
+        break;
+    case 7:
+        document.write('Seven');
+        break;
+    case 8:
+        document.write('Eight');
+        break;
+    case 9:
+        document.write('Nine');
+        break;
+    default:
+        document.write('Not a digit');
 }
