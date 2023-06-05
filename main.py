45ebfaeb306056c43282b57de3c783795e9c1c3b
Jeong Taek <imjt9345@naver.com>
2023-06-05 14:42:31 +0900

    명시적 형변환 실습

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-# 암시적 타입 변환
+# 명시적 타입 변환
+
 a = 5
 b = 2
-print(b, type(b))
 value = a / b
-print(value)
 print(type(value))
-
-x = 10
-y = 5.5
-total = x + y
-print(total)
-print(type(total))
-
-j = 'Hello'
-k = 'like lion'
-p = j + k
-print(p)
-print(type(p))
+int_value = int(value)
+print(int_value, type(int_value))
 
 q = 20
 u = '10'
-r = q + u
-print(r)
+r = q + int(u)
+print(r, type(r))
+r = str(q) + u
+print(r, type(r))
+
+n1 = 10.76
+vn1 = int(n1)
+print(vn1, type(vn1))
+
+n5 = '멋쟁이 사자'
+vn5 = tuple(n5)
+print(vn5, type(vn5))
