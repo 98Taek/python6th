f5e31a4b7d10508b0a33845cd68031d218ffab06
Jeong Taek <imjt9345@naver.com>
2023-06-07 13:26:02 +0900

    배열 슬라이싱 실습

--- main.py
+++ main.py
@@ -1,53 +1,38 @@
 from array import *
-stu_roll = array('i', [101, 102, 103, 104, 105])
+
+stu_roll = array('i', [101, 102, 103, 104, 105, 106, 107])
+
+print('배열 슬라이싱')
 n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
+for i in range(n):
+    print(i, '=', stu_roll[i])
 
-print('Array After Insert')
-stu_roll.insert(1, 106)
-stu_roll.insert(3, 107)
-n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
+print('1:5까지')
+a = stu_roll[1:5]
+for i in a:
+    print(i)
 
-# print('Array After Remove')
-# stu_roll.remove(101)
-# n = len(stu_roll)
-# i = 0
-# while i < n:
-#     print(stu_roll[i])
-#     i += 1
+print('0부터 끝까지')
+b = stu_roll[0:]
+for i in b:
+    print(i)
 
-# print('Array After Pop')
-# element = stu_roll.pop()
-# print('element:', element)
-# n = len(stu_roll)
-# i = 0
-# while i < n:
-#     print(stu_roll[i])
-#     i += 1
+print('처음부터 5번째가지')
+c = stu_roll[:5]
+for i in c:
+    print(i)
 
-print('Array index() 메서드')
-print(stu_roll.index(101))
+print('마지막 요소 4개 출력')
+d = stu_roll[-4:]
+for i in d:
+    print(i)
 
-print('extend() 메서드')
-arr = array('i', [201, 208, 209])
-stu_roll.extend(arr)
-n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
+print('처음부터 6번째까지 2개씩 건너뛰어 출력')
+e = stu_roll[:7:2]
+for i in e:
+    print(i)
 
-print('reverse() 메서드')
-stu_roll.reverse()
-n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
+print('마지막 5개 요소중 오른쪽으로부터 3개의 요소를 출력')
+f = stu_roll[-5:-3]
+for i in f:
+    print(i)
