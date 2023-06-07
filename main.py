87bbb33b9431a244691901ed60c0888219721d93
Jeong Taek <imjt9345@naver.com>
2023-06-07 13:41:34 +0900

    문자열 실습

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
-from array import *
+str1 = 'LikeLion'
+str2 = "LikeLion"
+str3 = '''
+동해물과 백두산이
+'''
+print(str1)
+print(str2)
+print(str3)
 
-stu_roll = array('i', [101, 102, 103, 104, 105, 106, 107])
+str5 = 'Hello "Like Lion" How are you'
+str6 = "Hello 'Like Lion' How are you"
+print(str5)
+print(str6)
 
-print('배열 슬라이싱')
-n = len(stu_roll)
-for i in range(n):
-    print(i, '=', stu_roll[i])
-
-print('1:5까지')
-a = stu_roll[1:5]
-for i in a:
-    print(i)
-
-print('0부터 끝까지')
-b = stu_roll[0:]
-for i in b:
-    print(i)
-
-print('처음부터 5번째가지')
-c = stu_roll[:5]
-for i in c:
-    print(i)
-
-print('마지막 요소 4개 출력')
-d = stu_roll[-4:]
-for i in d:
-    print(i)
-
-print('처음부터 6번째까지 2개씩 건너뛰어 출력')
-e = stu_roll[:7:2]
-for i in e:
-    print(i)
-
-print('마지막 5개 요소중 오른쪽으로부터 3개의 요소를 출력')
-f = stu_roll[-5:-3]
-for i in f:
-    print(i)
+str7 = 'Hello \nHow are you'
+str8 = 'Hello \\nHow are you'
+str9 = r'Hello \nHow are you'
+print(str7)
+print(str8)
+print(str9)
