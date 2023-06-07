766941823f124ebc383620dd444f754afafb81ae
Jeong Taek <imjt9345@naver.com>
2023-06-07 11:21:05 +0900

    기초 배열 실습

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-# pass문 실습
-a = 5
-if a < 6:
-    pass
-else:
-    print('6보다 큼')
+import array
+stu_roll = array.array('i', [101, 102, 103, 104, 105])
+print(stu_roll[0])
+print(stu_roll[1])
+print(stu_roll[2])
+print(stu_roll[3])
+print(stu_roll[4])
+
+print()
+print('for in 사용')
+for element in stu_roll:
+    print(element)
+
+print()
+print('인덱스를 이용한 순회')
+n = len(stu_roll)
+for i in range(n):
+    print(i, '=', stu_roll[i])
+
+print()
+print('인덱스를 사용한 while 루프 배열 순회')
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
