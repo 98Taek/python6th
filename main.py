f9d9a830dd90540de6514c3d05535c7d2c8e7561
Jeong Taek <imjt9345@naver.com>
2023-06-07 13:05:18 +0900

    index() extend() reverse() 배열 실습

--- main.py
+++ main.py
@@ -23,9 +23,29 @@ while i < n:
 #     print(stu_roll[i])
 #     i += 1
 
-print('Array After Pop')
-element = stu_roll.pop()
-print('element:', element)
+# print('Array After Pop')
+# element = stu_roll.pop()
+# print('element:', element)
+# n = len(stu_roll)
+# i = 0
+# while i < n:
+#     print(stu_roll[i])
+#     i += 1
+
+print('Array index() 메서드')
+print(stu_roll.index(101))
+
+print('extend() 메서드')
+arr = array('i', [201, 208, 209])
+stu_roll.extend(arr)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+print('reverse() 메서드')
+stu_roll.reverse()
 n = len(stu_roll)
 i = 0
 while i < n:
