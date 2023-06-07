56ea860d2a7150d2a2c5e4ec5c86db6470a25517
Jeong Taek <imjt9345@naver.com>
2023-06-07 10:28:53 +0900

    range 실습

--- main.py
+++ main.py
@@ -1,51 +1,25 @@
-i = 0
-while i < 10:
-    i += 1
-    print('i:', i)
-else:
-    print('else')
-
-# while True:
-#     a = input('Enter a number:')
-#     if a == 0:
-#         break
-#     print('a')
-# else:
-#     print('else')
-
-a = 2
-while a <= 20:
-    print(a)
-    a += 2
-print('코드 종료')
-
-a = 1
-while a <= 10:
-    print(a)
-    a += 1
-else:
-    print('while 조건이 거짓')
-print('코드 종료')
-
-# while True:
-#     print('멋쟁이 사자')
-# print('코드 종료')
-
-i = 0
-while True:
-    i += 1
+for i in range(5):
     print(i)
-    if i == 5:
-        break
-print('코드 종료')
 
-# 중첩
-i = 1
-while i <= 3:
-    print('Outer Loop', i)
-    i += 1
-    j = 1
-    while j <= 5:
-        print('Inner Loop', j)
-        j += 1
-print('코드 종료')
+for i in range(2, 7):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
+
+for i in range(-1, -10, -2):
+    print(i)
+
+a = range(5)
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+r = range(5, 0, -1)
+print(r[0])
+print(r[1])
+print(r[2])
+print(r[3])
+print(r[4])
