e7b7214f16414e79b5ff5df641ef0222ecc4f038
Jeong Taek <imjt9345@naver.com>
2023-06-07 11:40:50 +0900

    배열 실습

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-import array
-stu_roll = array.array('i', [101, 102, 103, 104, 105])
-print(stu_roll[0])
-print(stu_roll[1])
-print(stu_roll[2])
-print(stu_roll[3])
-print(stu_roll[4])
-
-print()
-print('for in 사용')
-for element in stu_roll:
-    print(element)
-
-print()
-print('인덱스를 이용한 순회')
+from array import *
+stu_roll = array('i', [101, 102, 103, 104, 105])
 n = len(stu_roll)
-for i in range(n):
-    print(i, '=', stu_roll[i])
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
 
-print()
-print('인덱스를 사용한 while 루프 배열 순회')
+print('Array After Insert')
+stu_roll.insert(1, 106)
+stu_roll.insert(3, 107)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll[i])
+    i += 1
+
+# print('Array After Remove')
+# stu_roll.remove(101)
+# n = len(stu_roll)
+# i = 0
+# while i < n:
+#     print(stu_roll[i])
+#     i += 1
+
+print('Array After Pop')
+element = stu_roll.pop()
+print('element:', element)
+n = len(stu_roll)
 i = 0
 while i < n:
     print(stu_roll[i])
