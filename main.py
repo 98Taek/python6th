dfd02955e7e7015435a9dcabbf70f78960733962
Jeong Taek <imjt9345@naver.com>
2023-06-07 14:24:45 +0900

    문자열 strip() 실습

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-s = 'Hello world'
-print(s.upper())
-print(s.lower())
-print(s.swapcase())
-print(s.title())
-print(s.isupper())
-print(s.islower())
-print(s.istitle())
-print(s.isdigit())
-print(s.isalpha())
+s = ' Hello world '
+
+print(s.lstrip())
+print(s.rstrip())
+print(s.strip())
