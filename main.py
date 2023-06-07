acf5206bc5fb1150088a44b5256cbc5bf718a7cc
Jeong Taek <imjt9345@naver.com>
2023-06-07 15:28:35 +0900

    중첩 함수 실습

--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-def add():
-    x = 10
-    y = 20
-    c = x + y
-    return c
+def disp():
+    def show():
+        return 'Show Function'
+    result = show() + 'Disp Function'
+    return result
 
 
-sum1 = add()
-print(sum1)
-
-def add():
-    x = 10
-    y = 40
-    return x + y
-
-
-sum2 = add()
-print(sum2)
-
-def add(y):
-    x = 10
-    return x + y
-
-
-sum3 = add(10)
-print(sum3)
-
-def add(y):
-    x = 10
-    c = x + y
-    d = y - x
-    return c, d, 50
-
-
-sum4, sub1, a = add(20)
-print(sum4, sub1, a)
+print(disp())
