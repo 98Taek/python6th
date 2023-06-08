d454202053b780739cdbd7167062f6d32e0db156
Jeong Taek <imjt9345@naver.com>
2023-06-08 10:33:40 +0900

    가변 키워드 인자 실습

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-# Example
-
-def add(x, y):
-    z = x + y
+def add(**num):
+    z = num['a'] + num['b'] + num['c']
     print('Addition:', z)
 
 
-add(5, 2)
+add(a=5, b=2, c=4, d=5)
 
 
-def add(*num):
-    z = num[0] + num[1] + num[2]
+def add(x, **num):
+    z = x + num['a'] + num['b'] + num['c']
     print('Addition:', z)
 
 
-add(5, 2, 4)
-
-
-def add(x, *num):
-    z = x + num[0] + num[1]
-    print('Addition x *:', z)
-
-
-add(5, 2, 4)
+add(3, a=5, b=2, c=4, d=5)
