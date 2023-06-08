0ee5460e305dca1b4500b45f37edb399249ea841
Jeong Taek <imjt9345@naver.com>
2023-06-08 14:22:24 +0900

    Set 실습

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ a = {10, 20, 30, 'abcd', '하이', 40}
 a = {10, 20, 30, 'abcd', '하이', 40, 10, 20}
 print(a)
 
+new_set = a.copy()
+print(new_set)
+
 b = set()
 print(type(b))
 
@@ -14,6 +17,18 @@ a.discard(10)
 a.discard(30)
 print(a)
 
-new_set = a.copy()
-new_set.clear()
-print(new_set)
+intersection_a = a.intersection(new_set, a, b)
+print(intersection_a)
+
+union_a = a.union(new_set)
+print('union_a =', union_a)
+
+diff_a = a.difference(new_set)
+print('diff_a =', diff_a)
+
+print(b.issubset(a))
+
+print(a.issuperset(b))
+
+sym_a = a.symmetric_difference(new_set)
+print('sym_a =', sym_a)
