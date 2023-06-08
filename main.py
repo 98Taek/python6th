edbd3e15322f2118cce88e85895fc40c470445ce
Jeong Taek <imjt9345@naver.com>
2023-06-08 13:12:09 +0900

    제네레이터 실습2

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+def generate_alphabet(start_letter, last_letter):
+    start = ord(start_letter)
+    end = ord(last_letter)
+    while start <= end:
+        yield chr(start)
+        start += 1
 
 
-runner = fibonacci(10)
+runner = generate_alphabet('A', 'F')
 
-print(next(runner))
-print(next(runner))
-
-for num in runner:
-    print(num)
+for letter in runner:
+    print(letter)
