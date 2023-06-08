6654e0d2c0ba0044944150552c55a6cfb2df8ead
Jeong Taek <imjt9345@naver.com>
2023-06-08 14:11:30 +0900

    기본 Set 실습

--- main.py
+++ main.py
@@ -1,44 +1,19 @@
-b = (10)
-c = (10,)
-print(b)
-print(c)
+a = {10, 20, 30}
+a = {10, 20, 30, 'abcd', '하이', 40}
+a = {10, 20, 30, 'abcd', '하이', 40, 10, 20}
+print(a)
 
-d = (10, 20, 30, 40)
-e = (10, 20, -50, 21.3, '멋쟁이사자')
-f = 1, 10, 20, -50, 21.3, '멋쟁이사자'
+b = set()
+print(type(b))
 
-print(d)
-print(e)
-print(f)
+a.add(50)
+a.update([10, 60])
+print(a)
+a.remove(10)
+a.discard(10)
+a.discard(30)
+print(a)
 
-print(f[1])
-print(f[2])
-print(f[3])
-print(f[4])
-
-print(f[:3])
-print(f[1:4])
-print(f[3:])
-
-print(c + d)
-print(c + f)
-
-g = c + f
-print(g)
-print(f * 5)
-print(-10 in f)
-h = 10, 20, -50, 20
-print(min(h), max(h))
-print(h.count(20))
-print(h.index(20))
-sorted_a = sorted(h)
-print(sorted_a)
-
-list_h = list(h)
-print(list_h)
-
-tuple_h = tuple(list_h)
-print(tuple_h)
-
-nested_tuple = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
-print(nested_tuple)
+new_set = a.copy()
+new_set.clear()
+print(new_set)
