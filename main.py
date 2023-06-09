3ee7ef281f8ca8d6fcac47650db051ca4ca18e93
Jeong Taek <imjt9345@naver.com>
2023-06-09 09:47:37 +0900

    클래스 네임스페이스, 인스턴스

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from array import *
+class Mobile:
+    fp = 'yes'
 
 
-def show(ar):
-    print('Passed Array ar:', ar)
-    print(type(ar))
-    for i in ar:
-        print(i)
-    return ar
+realme = Mobile()
+redme = Mobile()
+geek = Mobile()
 
+print(Mobile.fp)
+print(realme.fp)
+print(redme.fp)
+print(geek.fp)
 
-print()
-a = array('i', [101, 102, 103, 104])
-y = show(a)
-print('Return Array Y:', y)
-for i in y:
-    print(i)
+Mobile.fp = 'no'
+print(Mobile.fp)
+print(realme.fp)
+print(redme.fp)
+print(geek.fp)
+
+realme.fp = 'Not Working'
+print(Mobile.fp)
+print(realme.fp)
+print(redme.fp)
+print(geek.fp)
