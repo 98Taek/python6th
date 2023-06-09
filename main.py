e07b9614737cd4be89080baf95ce3545ba32fd1a
Jeong Taek <imjt9345@naver.com>
2023-06-09 13:41:28 +0900

    파일 위치 확인 오타 수정

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-file_object = open('new_example.txt', 'w')
+file_object = open('example.txt', 'r')
 
-content = "This is a new file\nPython is fun"
+print('현재 파일 위치 확인')
+position = file_object.tell()
+print('Current position:', position)
 
-file_object.write(content)
+print('파일 포인터 위치 변경')
+file_object.seek(7)
+
+print('변경된 위치 확인')
+position = file_object.tell()
+print('New position:', position)
 
 file_object.close()
