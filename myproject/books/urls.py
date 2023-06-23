47fe375de47ea97b934b3b90577a49a984398170
Jeong Taek <imjt9345@naver.com>
2023-06-23 13:23:58 +0900

    author_detail.html, publisher_detail.html 추가

--- myproject/books/urls.py
+++ myproject/books/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('author/', views.AuthorList.as_view(), name='author_list'),
     path('publisher/', views.PublisherList.as_view(), name='publisher_list'),
     path('book/<int:pk>', views.BookDetail.as_view(), name='book_detail'),
+    path('author/<int:pk>', views.AuthorDetail.as_view(), name='author_detail'),
+    path('publisher/<int:pk>', views.PublisherDetail.as_view(), name='publisher_detail'),
 ]
